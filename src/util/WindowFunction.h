83e86c9a5f4c5133f72fff017870b4263827333f
Marcelo Y. Matuda <src@mym.eng.br>
2018-07-24 21:27:24 -0300

    WindowFunction: Implement windows.

--- src/util/WindowFunction.h
+++ src/util/WindowFunction.h
@@ -20,6 +20,7 @@
 
 #include <vector>
 
+#include "Exception.h"
 #include "Util.h"
 
 
@@ -27,13 +28,188 @@
 namespace Lab {
 namespace WindowFunction {
 
+// n can be a non-integer.
 template<typename FloatType>
 void
-hamming(int n, std::vector<FloatType>& w)
+fadeIn(double n, FloatType* data)
 {
+	const double c1 = 1.0 / n;
+	for (unsigned int i = 0; i < n; ++i) {
+		data[i] = i * c1;
+	}
+}
+
+// The first zeroed element is not included in the returned data.
+// n can be a non-integer.
+template<typename FloatType>
+void
+fadeIn2(double n, FloatType* data)
+{
+	const double c1 = 1.0 / (n + 1.0);
+	for (unsigned int i = 0; i < n; ++i) {
+		data[i] = (i + 1.0) * c1;
+	}
+}
+
+// n can be a non-integer.
+template<typename FloatType>
+void
+fadeOut(double n, FloatType* data)
+{
+	const double c1 = 1.0 / n;
+	for (unsigned int i = 0; i < n; ++i) {
+		data[i] = (n - i - 1.0) * c1;
+	}
+}
+
+// The last zeroed element is not included in the returned data.
+// n can be a non-integer.
+template<typename FloatType>
+void
+fadeOut2(double n, FloatType* data)
+{
+	const double c1 = 1.0 / (n + 1.0);
+	for (unsigned int i = 0; i < n; ++i) {
+		data[i] = (n - i) * c1;
+	}
+}
+
+template<typename FloatType>
+void
+rectangular(unsigned int n, std::vector<FloatType>& w)
+{
+	if (n < 2U) THROW_EXCEPTION(InvalidParameterException, "The window function size must be >= 2.");
+	w.assign(n, 1.0);
+}
+
+template<typename FloatType>
+void
+trapezoidal(unsigned int nFadeIn, unsigned int nTop, unsigned int nFadeOut, std::vector<FloatType>& w)
+{
+	if (n < 2U) THROW_EXCEPTION(InvalidParameterException, "The window function size must be >= 2.");
+	w.resize(nFadein + nTop + nFadeout);
+	fadeIn(nFadeIn, &w[0]);
+	for (unsigned int i = nFadeIn; i < nFadeIn + nTop; ++i) {
+		w[i] = 1.0;
+	}
+	fadeOut(nFadeOut, &w[nFadeIn + nTop]);
+}
+
+template<typename FloatType>
+void
+# The first and the last zeroed elements are not included in the returned array.
+trapezoidal2(unsigned int nFadein, unsigned int n_top, unsigned int nFadeout)
+{
+	if (n < 2U) THROW_EXCEPTION(InvalidParameterException, "The window function size must be >= 2.");
+	w.resize(nFadein + nTop + nFadeout);
+	fadeIn2(nFadeIn, &w[0]);
+	for (unsigned int i = nFadeIn; i < nFadeIn + nTop; ++i) {
+		w[i] = 1.0;
+	}
+	fadeOut2(nFadeOut, &w[nFadeIn + nTop]);
+}
+
+template<typename FloatType>
+void
+triangular(unsigned int n, std::vector<FloatType>& w)
+{
+	if (n < 2U) THROW_EXCEPTION(InvalidParameterException, "The window function size must be >= 2.");
 	w.resize(n);
-	for (int i = 0; i < n; ++i) {
-		w[i] = 0.54 - 0.46 * std::cos((2.0 * PI * i) / (n - 1));
+	const double halfWidth = 0.5 * (n - 1.0);
+	if (n % 2 == 0) {
+		fadeIn(halfWidth, &w[0]);
+		fadeIn(halfWidth, &w[n / 2]);
+	} else {
+		fadeIn(halfWidth, &w[0]);
+		const unsigned int n2 = (n - 1) / 2;
+		&w[n2] = 1.0;
+		fadeIn(halfWidth, &w[n2 + 1]);
+	}
+}
+
+// The first and the last zeroed elements are not included in the returned array.
+template<typename FloatType>
+void
+triangular2(unsigned int n, std::vector<FloatType>& w)
+{
+	if (n < 2U) THROW_EXCEPTION(InvalidParameterException, "The window function size must be >= 2.");
+	w.resize(n);
+	const double halfWidth = 0.5 * (n - 1.0);
+	if (n % 2 == 0) {
+		fadeIn2(halfWidth, &w[0]);
+		fadeIn2(halfWidth, &w[n / 2]);
+	} else {
+		fadeIn2(halfWidth, &w[0]);
+		const unsigned int n2 = (n - 1) / 2;
+		&w[n2] = 1.0;
+		fadeIn2(halfWidth, &w[n2 + 1]);
+	}
+}
+
+template<typename FloatType>
+void
+hanning(unsigned int n, std::vector<FloatType>& w)
+{
+	if (n < 2U) THROW_EXCEPTION(InvalidParameterException, "The window function size must be >= 2.");
+	w.resize(n);
+	const double c1 = 1.0 / (n - 1.0);
+	for (unsigned int i = 0; i < n; ++i) {
+		const double c2 = i * c1;
+		w[i] = 0.5 - 0.5 * std::cos(2.0 * PI * c2);
+	}
+}
+
+// The first and the last zeroed elements are not included in the returned array.
+template<typename FloatType>
+void
+hanning2(unsigned int n, std::vector<FloatType>& w)
+{
+	if (n < 2U) THROW_EXCEPTION(InvalidParameterException, "The window function size must be >= 2.");
+	w.resize(n);
+	const double c1 = 1.0 / (n + 1.0);
+	for (unsigned int i = 0; i < n; ++i) {
+		const double c2 = (i + 1.0) * c1;
+		w[i] = 0.5 - 0.5 * std::cos(2.0 * PI * c2);
+	}
+}
+
+template<typename FloatType>
+void
+hamming(unsigned int n, std::vector<FloatType>& w)
+{
+	if (n < 2U) THROW_EXCEPTION(InvalidParameterException, "The window function size must be >= 2.");
+	w.resize(n);
+	const double c1 = 1.0 / (n - 1.0);
+	for (unsigned int i = 0; i < n; ++i) {
+		const double c2 = i * c1;
+		w[i] = 0.54 - 0.46 * std::cos(2.0 * PI * c2);
+	}
+}
+
+template<typename FloatType>
+void
+blackman(unsigned int n, std::vector<FloatType>& w)
+{
+	if (n < 2U) THROW_EXCEPTION(InvalidParameterException, "The window function size must be >= 2.");
+	w.resize(n);
+	const double c1 = 1.0 / (n - 1.0);
+	for (unsigned int i = 0; i < n; ++i) {
+		const double c2 = i * c1;
+		w[i] = 0.42 - 0.5 * std::cos(2.0 * PI * c2) + 0.08 * std::cos(4.0 * PI * c2);
+	}
+}
+
+// The first and the last zeroed elements are not included in the returned array.
+template<typename FloatType>
+void
+blackman2(unsigned int n, std::vector<FloatType>& w)
+{
+	if (n < 2U) THROW_EXCEPTION(InvalidParameterException, "The window function size must be >= 2.");
+	w.resize(n);
+	const double c1 = 1.0 / (n + 1.0);
+	for (unsigned int i = 0; i < n; ++i) {
+		const double c2 = (i + 1.0) * c1;
+		w[i] = 0.42 - 0.5 * std::cos(2.0 * PI * c2) + 0.08 * std::cos(4.0 * PI * c2);
 	}
 }
 
