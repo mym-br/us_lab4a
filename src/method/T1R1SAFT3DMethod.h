52998deaa502d1773b57df6ff88792515fdddc16
Marcelo Y. Matuda <src@mym.eng.br>
2019-02-11 19:12:45 -0200

    T1R1SAFT3DMethod: Remove useless copy.

--- src/method/T1R1SAFT3DMethod.h
+++ src/method/T1R1SAFT3DMethod.h
@@ -62,7 +62,6 @@ private:
 
 	Project& project_;
 	Matrix<XYZValueFactor<FloatType>> gridData_;
-	Project::GridDataType projGridData_;
 	std::vector<XYZ<float>> pointList_;
 	Figure::Visualization visual_;
 };
@@ -98,8 +97,7 @@ T1R1SAFT3DMethod<FloatType>::useCoherenceFactor(FloatType valueScale, const std:
 	LOG_DEBUG << "Saving the CF image...";
 	project_.saveHDF5(gridData_, outputDir + "/image_cf", "cf", Util::CopyValueOp());
 
-	Util::copyXYZValue(gridData_, projGridData_);
-	project_.showFigure3D(2, "Coherence factor image", &projGridData_, &pointList_,
+	project_.showFigure3D(2, "Coherence factor image", &gridData_, &pointList_,
 				true, Figure::VISUALIZATION_RECTIFIED_LOG, Figure::COLORMAP_VIRIDIS, valueScale);
 }
 
@@ -113,8 +111,7 @@ T1R1SAFT3DMethod<FloatType>::process(FloatType valueScale, ArrayProcessor<FloatT
 
 	project_.saveImageToHDF5(gridData_, outputDir);
 
-	Util::copyXYZValue(gridData_, projGridData_);
-	project_.showFigure3D(1, "Raw image", &projGridData_, &pointList_,
+	project_.showFigure3D(1, "Raw image", &gridData_, &pointList_,
 				true, visual_, Figure::COLORMAP_VIRIDIS, valueScale);
 
 	LOG_DEBUG << ">>> Acquisition + processing time: " << tProc.getTime();
