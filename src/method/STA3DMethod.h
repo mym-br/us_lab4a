0dcafa7ebd90b867f3ea267e22b34fa6ee72c23d
Marcelo Y. Matuda <src@mym.eng.br>
2019-02-11 19:02:09 -0200

    STA3DMethod: Remove useless copy.

--- src/method/STA3DMethod.h
+++ src/method/STA3DMethod.h
@@ -61,7 +61,6 @@ private:
 
 	Project& project_;
 	Matrix<XYZValueFactor<FloatType>> gridData_;
-	Project::GridDataType projGridData_;
 	std::vector<XYZ<float>> pointList_;
 };
 
@@ -95,8 +94,7 @@ STA3DMethod<FloatType>::useCoherenceFactor(FloatType valueScale, const std::stri
 	LOG_DEBUG << "Saving the CF image...";
 	project_.saveHDF5(gridData_, outputDir + "/image_cf", "cf", Util::CopyValueOp());
 
-	Util::copyXYZValue(gridData_, projGridData_);
-	project_.showFigure3D(2, "Coherence factor image", &projGridData_, &pointList_,
+	project_.showFigure3D(2, "Coherence factor image", &gridData_, &pointList_,
 				true, Figure::VISUALIZATION_RECTIFIED_LOG, Figure::COLORMAP_VIRIDIS, valueScale);
 }
 
@@ -110,8 +108,7 @@ STA3DMethod<FloatType>::process(FloatType valueScale, ArrayProcessor<FloatType>&
 
 	project_.saveImageToHDF5(gridData_, outputDir);
 
-	Util::copyXYZValue(gridData_, projGridData_);
-	project_.showFigure3D(1, "Raw image", &projGridData_, &pointList_,
+	project_.showFigure3D(1, "Raw image", &gridData_, &pointList_,
 				true, Figure::VISUALIZATION_RECTIFIED_LOG, Figure::COLORMAP_VIRIDIS, valueScale);
 
 	LOG_DEBUG << ">>> Acquisition + processing time: " << tProc.getTime();
